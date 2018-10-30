5ef9f88184e2c31cda958f57046f198d212fbca4
Jeff Gonzalez <likwid@gmail.com>
2018-10-30 09:08:48 -0500

    Reorganize files

--- zsh/zsh-options.zsh
+++ zsh/zsh-options.zsh
@@ -1,7 +1,7 @@
 ZSH_THEME="robbyrussell"
 ZSH_CUSTOM=$HOME/.zsh
+
 # Uncomment following line if you want to disable command autocorrection
 DISABLE_CORRECTION="true"
 setopt HIST_IGNORE_ALL_DUPS
 setopt INC_APPEND_HISTORY
-
