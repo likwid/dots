871bc1b2c0d5631bb2aed5699bef2b837a249318
Jeff Gonzalez <likwid@gmail.com>
2019-03-18 10:32:01 -0500

    Remove old prompt

--- zsh/zsh-options.zsh
+++
@@ -1,7 +0,0 @@
-ZSH_THEME="spaceship"
-ZSH_CUSTOM=$HOME/.zsh
-
-# Uncomment following line if you want to disable command autocorrection
-DISABLE_CORRECTION="true"
-setopt HIST_IGNORE_ALL_DUPS
-setopt INC_APPEND_HISTORY
