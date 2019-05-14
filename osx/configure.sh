6f7da7fbf18583b70e338c0af6b8f5e70cba43cb
Jeff Gonzalez <likwid@gmail.com>
2019-05-14 14:00:09 -0700

    Add alice as a company

--- osx/configure.sh
+++ osx/configure.sh
@@ -48,7 +48,7 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 40
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
