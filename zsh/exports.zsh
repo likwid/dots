58e12f5be00858cca4267efb0fc32dd95376abe0
Jeff Gonzalez <likwid@gmail.com>
2019-07-11 12:15:33 -0500

    Add DEFAULT_USER env var

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,3 +1,4 @@
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home/
 export TZ=/usr/share/zoneinfo/US/Central
+export DEFAULT_USER=jgonzalez
